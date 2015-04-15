1463952c7acab41f3344eba19a1bc7170319b04d
Labuser <Labuser@wtk316-32.cec.wustl.edu>
2015-04-14 19:51:21 -0500

    Initial Commit

---
+++ Mazour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mazour
+//
+//  Created by Labuser on 4/14/15.
+//  Copyright (c) 2015 wustl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
