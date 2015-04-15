6c14e9ef31b989f246d7647f92cb4fe5467b8030
dmazour <dmazour@gmail.com>
2015-04-14 20:18:53 -0500

    4-14-2015

--- Mazour/ViewController.swift
+++ Mazour/ViewController.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +22,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet var tableView: UITableView!
+    
+    let textCellIdentifier = "TextCell"
+    
+    var providers: [String] = ["Welcome!"]
+    // if(beacon){
+    //      providers.append(place.name)
+    //      reloadData      }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return providers.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(textCellIdentifier, forIndexPath: indexPath) as UITableViewCell
+        
+        let row = indexPath.row
+        cell.textLabel?.text = providers[row]
+        
+        return cell
+    }
 
 }
 
